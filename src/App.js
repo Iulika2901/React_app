2b7592909a339caf4ad9024c9af85648d7b7a53d
Bacriu_Iuliana <84437252+Iulika2901@users.noreply.github.com>
2024-08-04 18:55:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Item from './MyItem';
 
+class StarWars extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      height: null,
+      homeworld: null,
+      films: [],
+    };
+  }
 
+  getNewCaracter() {
+    const randomNumber=Math.round(Math.random()*82)
+    console.log("Get New Character from a button")
+    const url = "https://swapi.dev/api/people/${randomNumber}/"
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        
+        this.setState({
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          films: data.films,
+          loadedCharacter: true,
+        });
+
+      })
+   
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.loadChararcter && 
+           <div>
+                 <h1>{this.state.name}</h1>
+                  <p>{this.state.height} cm</p>
+                   <p>Homeworld: {this.state.homeworld}</p>
+                   <ul>
+                     {this.state.films.map((film, index) => (
+                    <li key={index}>{film}</li>
+                   ))}
+                 </ul>
+           </div>
+        }
+    
+        <button
+          type="button"
+          onClick={() => this.getNewCaracter()}
+          className="btn"
+        >
+          Randomize Character
+        </button>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Item name="Gully`"/>
-        <Item name="Sun"/>
-        <Item name="Snail"/>
-        <p>
-          <Item/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StarWars />
       </header>
     </div>
   );
